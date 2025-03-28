4100f264b24ee54461f2b829dd83a6832f1565b2
maksimka1234567 <povaliaevmaksim@yandex.ru>
2025-03-28 16:31:23 +0400

    update 2

--- main.py
+++ main.py
@@ -45,5 +45,12 @@ def login():
         return "Доступ открыт"
 
 
+@app.route('/distribution')
+def distribution():
+    params = {'astronauts': ['Ридли Скотт', 'Энди Уир', 'Марк Уотни',
+         'Венката Капур', 'Тедди Сандерс', 'Шон Бин']}
+    return render_template('distribution.html', **params)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
